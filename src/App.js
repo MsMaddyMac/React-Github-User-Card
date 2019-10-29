b9a90c554d6798d4d6c769db0698806837bd2e8a
Maddy <mmcintosh2k14@gmail.com>
2019-10-29 16:48:46 -0400

    adds axios call to retrieveuser info and starts github user card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import GithubUserCard from './components/GithubUserCard';
+
+
+class App extends Component {
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/MsMaddyMac')
+    .then(response => {
+      console.log(response);
+    })
+  
+    .catch(error => {
+    console.log("No information found.", error);
+    });
+  }
+
+  render() {
+    console.log('App is rendering...')
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Github Users</h1>
+        </header>
+        <div className="user-list">
+          {/* <GithubUserList /> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
