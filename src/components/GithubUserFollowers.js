13b78d2db8f221b783d042efe98fb550b1d09311
Maddy <mmcintosh2k14@gmail.com>
2019-10-30 12:00:53 -0400

    adds links to follower cards and more css styling

--- src/components/GithubUserFollowers.js
+++ src/components/GithubUserFollowers.js
@@ -6,10 +6,12 @@ const GithubUserFollowers = props => {
     return (
         <>
          {props.followers.map(follower => (
-             <div key={follower.id} className="follower">
-                 <img src={follower.avatar_url} alt={follower.login} />
-                 <h3>{follower.login}</h3>
-            </div>
+             <a href={follower.html_url}>
+                <div key={follower.id} className="follower">
+                    <img src={follower.avatar_url} alt={follower.login} />
+                    <h3>{follower.login}</h3>
+                </div>
+            </a>
          ))}
         </>
     );
